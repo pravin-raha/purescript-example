d6f9ecf995b9768a54328ee8993eeba0ee3727bd
Pravin <pravinrahangdale41@gmail.com>
2019-09-28 23:33:34 +0530

    Added halogen dynamic html example

---
+++ src/Html/DynamicHtml.purs
@@ -0,0 +1,35 @@
+module Html.DynamicHtml where
+
+import Prelude
+
+import Data.Const (Const)
+import Effect (Effect)
+import Effect.Aff (Aff, launchAff_)
+import Halogen (HalogenF(..))
+import Halogen as H
+import Halogen.Aff (awaitBody)
+import Halogen.HTML as HH
+import Halogen.VDom.Driver (runUI)
+
+type State = Int
+type DynamicHtml  =  H.ComponentHTML Unit () Aff
+type StateOnlyDynamicRenderer state = (state -> DynamicHtml)
+
+dynamicHtml :: State -> DynamicHtml
+dynamicHtml state = HH.div_ [ HH.text $ "This is text in a div!" <> show state]
+
+main :: Effect Unit
+main = launchAff_ do
+  body <- awaitBody
+  runUI (dynamicComponent 1 dynamicHtml) unit body    
+
+dynamicComponent :: forall state.
+       state
+    -> StateOnlyDynamicRenderer state
+    -> H.Component HH.HTML (Const Unit) Unit Void Aff
+dynamicComponent state renderHtml =
+  H.mkComponent
+    { initialState: const state
+    , render: \_ -> renderHtml state
+    , eval: H.mkEval H.defaultEval
+    }
